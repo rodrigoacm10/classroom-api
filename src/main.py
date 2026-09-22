e97863790d8653ccf20b253931a100ed8cad3e7f
Rodrigo Andrade <rodrigoacm10@gmail.com>
2026-09-22 18:34:33 -0300

    chore: dockerfiles to railway

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 from contextlib import asynccontextmanager
 import asyncio
+from pathlib import Path
 import sys
+
+sys.path.insert(0, str(Path(__file__).resolve().parent))
 
 if sys.platform == "win32":
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
